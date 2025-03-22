605c76b74f8d5f285893763fbb77337aeae5c978
Venxe <ivenxe@gmail.com>
2025-03-23 01:44:14 +0300

    Update install_win.ps1

--- installers/install_win.ps1
+++ installers/install_win.ps1
@@ -1,30 +1,27 @@
 $installDir = "$env:TEMP\wallpapers"
 $wallpapersDir = "C:\Users\$env:USERNAME\Pictures\Wallpapers"
 
-# Check if the directory exists, if not, create it
+# Ensure wallpapers directory exists
 if (-not (Test-Path $wallpapersDir)) {
     New-Item -ItemType Directory -Force -Path $wallpapersDir
 }
 
-# Cloning the repository
+# Clone the repository
 git clone --depth 1 https://github.com/Venxe/wallpapers.git $installDir
 
-# Moving the wallpapers
+# Move wallpapers to categorized directories
 Get-ChildItem -Path "$installDir\wallpapers" -Recurse -Include *.jpg, *.jpeg, *.png, *.webp | ForEach-Object {
-    $file = $_
-    $category = $file.DirectoryName.Replace("$installDir\wallpapers\", "")
+    $category = $_.DirectoryName.Replace("$installDir\wallpapers\", "")
     $destDir = "$wallpapersDir\$category"
 
-    # Check if the category directory exists, if not, create it
     if (-not (Test-Path $destDir)) {
         New-Item -ItemType Directory -Force -Path $destDir
     }
 
-    # Move the wallpaper file
-    Move-Item -Path $file.FullName -Destination $destDir
+    Move-Item -Path $_.FullName -Destination $destDir
 }
 
-# Clean up
+# Clean up temporary files
 Remove-Item -Recurse -Force $installDir
 
 Write-Host "Wallpaper installation complete."
