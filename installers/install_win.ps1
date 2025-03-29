f8b2ac69513f26783ad1b8e13e1ae04e95c631be
Venxe <ivenxe@gmail.com>
2025-03-29 22:28:01 +0300

    Update install_win.ps1

--- installers/install_win.ps1
+++ installers/install_win.ps1
@@ -33,5 +33,32 @@ catch {
 }
 finally {
     Remove-Item -Recurse -Force $installDir
-    Remove-Item -Path $MyInvocation.MyCommand.Path -Force
 }
+
+# WebP dosyaları için masaüstü arka planı olarak ayarlama özelliğini etkinleştirme
+if (-not ([System.Security.Principal.WindowsPrincipal]::new([System.Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator))) {
+    Start-Process PowerShell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    exit
+}
+
+$regFile = "$env:TEMP\webp-bg.reg"
+$regContent = @"
+Windows Registry Editor Version 5.00
+
+[HKEY_CLASSES_ROOT\SystemFileAssociations\.webp\Shell]
+
+[HKEY_CLASSES_ROOT\SystemFileAssociations\.webp\Shell\setdesktopwallpaper]
+@="Set as Desktop Wallpaper"
+"MultiSelectModel"="Player"
+"NeverDefault"=""
+
+[HKEY_CLASSES_ROOT\SystemFileAssociations\.webp\Shell\setdesktopwallpaper\Command]
+@="rundll32.exe %SystemRoot%\\System32\\shimgvw.dll,ImageView_SetWallpaper %1"
+"DelegateExecute"="{ff609cc7-d34d-4049-a1aa-2293517ffcc6}"
+"@
+
+$regContent | Set-Content -Path $regFile -Encoding ASCII
+Start-Process "regedit.exe" -ArgumentList "/s `"$regFile`"" -Wait -NoNewWindow
+
+# Son olarak, betiğin kendisini temizleme
+Remove-Item -Path $MyInvocation.MyCommand.Path -Force
