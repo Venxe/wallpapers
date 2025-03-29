4699caf226b31912f58a8a62ee9ce68b881b5934
Venxe <ivenxe@gmail.com>
2025-03-29 17:50:08 +0300

    Update install_win.ps1

--- installers/install_win.ps1
+++ installers/install_win.ps1
@@ -11,25 +11,33 @@ if (-not (Test-Path $wallpapersDir)) {
     New-Item -ItemType Directory -Force -Path $wallpapersDir
 }
 
-git clone --depth 1 https://github.com/Venxe/wallpapers.git $installDir
+try {
+    Write-Host "Cloning repository..." -ForegroundColor Cyan
+    git clone --depth 1 https://github.com/Venxe/wallpapers.git $installDir
 
-$null = Get-ChildItem -Path "$installDir\wallpapers" -Recurse -Include *.jpg, *.jpeg, *.png, *.webp | ForEach-Object {
-    $category = $_.DirectoryName.Replace("$installDir\wallpapers\", "")
-    $destDir = "$wallpapersDir\$category"
+    $null = Get-ChildItem -Path "$installDir\wallpapers" -Recurse -Include *.jpg, *.jpeg, *.png, *.webp | ForEach-Object {
+        $category = $_.DirectoryName.Replace("$installDir\wallpapers\", "")
+        $destDir = "$wallpapersDir\$category"
 
-    if (-not (Test-Path $destDir)) {
-        New-Item -ItemType Directory -Force -Path $destDir
+        if (-not (Test-Path $destDir)) {
+            New-Item -ItemType Directory -Force -Path $destDir
+        }
+
+        $destFile = "$destDir\$($_.Name)"
+
+        if (Test-Path $destFile) {
+            Write-Warning -ForegroundColor Yellow "File '$($_.Name)' already exists in the destination directory. Skipping."
+        } else {
+            Move-Item -Path $_.FullName -Destination $destDir
+        }
     }
 
-    $destFile = "$destDir\$($_.Name)"
-
-    if (Test-Path $destFile) {
-        Write-Warning "File '$($_.Name)' already exists in the destination directory. Skipping."
-    } else {
-        Move-Item -Path $_.FullName -Destination $destDir
-    }
+    Write-Host -ForegroundColor Green "Wallpaper installation complete."
 }
-
-Write-Host -ForegroundColor Green "Wallpaper installation complete."
-Remove-Item -Recurse -Force $installDir
-Remove-Item -Path $MyInvocation.MyCommand.Path -Force
+catch {
+    Write-Host -ForegroundColor Red "An error occurred: $_"
+}
+finally {
+    Remove-Item -Recurse -Force $installDir
+    Remove-Item -Path $MyInvocation.MyCommand.Path -Force
+}
