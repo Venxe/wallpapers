66c8bf3d476b73cc0ec60d0a3fd73862925d9466
Venxe <ivenxe@gmail.com>
2025-03-29 18:17:23 +0300

    Update install_win.ps1

--- installers/install_win.ps1
+++ installers/install_win.ps1
@@ -7,7 +7,7 @@ if (-not (Test-Path $wallpapersDir)) {
 
 try {
     Write-Host "Cloning repository..." -ForegroundColor Cyan
-    git clone --depth 1 https://github.com/Venxe/wallpapers.git $installDir
+    git clone --depth 1 https://github.com/Venxe/wallpapers.git $installDir | Out-Null
 
     Get-ChildItem -Path "$installDir\wallpapers" -Recurse -Include *.jpg, *.jpeg, *.png, *.webp | ForEach-Object {
         $category = $_.DirectoryName.Replace("$installDir\wallpapers\", "")
