96843cd7037e8f30047e1a9fe2cfb9c4cb364394
Venxe <ivenxe@gmail.com>
2025-03-29 18:35:43 +0300

    Update install_win.ps1

--- installers/install_win.ps1
+++ installers/install_win.ps1
@@ -1,35 +1,36 @@
 $installDir = "$env:TEMP\wallpapers"
-$wallpapersDir = ""
-
-if ($env:LANG -match "tr") {
-    $wallpapersDir = "$env:USERPROFILE\Resimler\Wallpapers"
-} else {
-    $wallpapersDir = "$env:USERPROFILE\Pictures\Wallpapers"
-}
+$wallpapersDir = if ($env:LANG -match "tr") { "$env:USERPROFILE\Resimler\Wallpapers" } else { "$env:USERPROFILE\Pictures\Wallpapers" }
 
 if (-not (Test-Path $wallpapersDir)) {
     New-Item -ItemType Directory -Force -Path $wallpapersDir
 }
 
-git clone --depth 1 https://github.com/Venxe/wallpapers.git $installDir
+try {
+    Write-Host "Cloning repository..." -ForegroundColor Cyan
+    git clone --depth 1 https://github.com/Venxe/wallpapers.git $installDir
 
-$null = Get-ChildItem -Path "$installDir\wallpapers" -Recurse -Include *.jpg, *.jpeg, *.png, *.webp | ForEach-Object {
-    $category = $_.DirectoryName.Replace("$installDir\wallpapers\", "")
-    $destDir = "$wallpapersDir\$category"
+    Get-ChildItem -Path "$installDir\wallpapers" -Recurse -Include *.jpg, *.jpeg, *.png, *.webp | ForEach-Object {
+        $category = $_.DirectoryName.Replace("$installDir\wallpapers\", "")
+        $destDir = "$wallpapersDir\$category"
 
-    if (-not (Test-Path $destDir)) {
-        New-Item -ItemType Directory -Force -Path $destDir
+        if (-not (Test-Path $destDir)) {
+            New-Item -ItemType Directory -Force -Path $destDir
+        }
+
+        $destFile = "$destDir\$($_.Name)"
+        if (Test-Path $destFile) {
+            Write-Host -ForegroundColor Yellow "File '$($_.Name)' already exists in the destination directory. Skipping."
+        } else {
+            Move-Item -Path $_.FullName -Destination $destDir
+        }
     }
 
-    $destFile = "$destDir\$($_.Name)"
-
-    if (Test-Path $destFile) {
-        Write-Warning "File '$($_.Name)' already exists in the destination directory. Skipping."
-    } else {
-        Move-Item -Path $_.FullName -Destination $destDir
-    }
+    Write-Host -ForegroundColor Green "Wallpaper installation complete."
 }
-
-Write-Host -ForegroundColor Green "Wallpaper installation complete."
-Remove-Item -Recurse -Force $installDir
-Remove-Item -Path $MyInvocation.MyCommand.Path -Force
+catch {
+    Write-Host -ForegroundColor Red "An error occurred during the wallpaper installation: $_"
+}
+finally {
+    Remove-Item -Recurse -Force $installDir
+    Remove-Item -Path $MyInvocation.MyCommand.Path -Force
+}
